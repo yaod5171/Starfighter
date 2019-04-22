24707dd40b03c4349e8b3fae00390aa877b391b9
dyaom <dyaomaster@yahoo.com>
2019-04-22 08:39:11 -0700

    Worked on MovingThing class

--- src/Starfighter/MovingThing.java
+++ src/Starfighter/MovingThing.java
@@ -3,88 +3,79 @@ package Starfighter;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.awt.Color;
 import java.awt.Graphics;
 
-public abstract class MovingThing implements Moveable
-{
-	private int xPos;
-	private int yPos;
-	private int width;
-	private int height;
+public abstract class MovingThing implements Moveable {
 
-	public MovingThing()
-	{
-		xPos = 10;
-		yPos = 10;
-		width = 10;
-		height = 10;
-	}
+    private int xPos;
+    private int yPos;
+    private int width;
+    private int height;
 
-	public MovingThing(int x, int y)
-	{
-		xPos = x;
-		yPos = y;
-		width = 10;
-		height = 10;
-	}
+    public MovingThing() {
+        xPos = 10;
+        yPos = 10;
+        width = 10;
+        height = 10;
+    }
 
-	public MovingThing(int x, int y, int w, int h)
-	{
-		//add code here
-	}
+    public MovingThing(int x, int y) {
+        xPos = x;
+        yPos = y;
+        width = 10;
+        height = 10;
+    }
 
-	public void setPos( int x, int y)
-	{
-		//add code here
-	}
+    public MovingThing(int x, int y, int w, int h) {
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
 
-	public void setX(int x)
-	{
-		//add code here
-	}
+    public void setPos(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
 
-	public void setY(int y)
-	{
-		//add code here
-	}
+    public void setX(int x) {
+        xPos = x;
+    }
 
-	public int getX()
-	{
-		return 0;   //finish this method
-	}
+    public void setY(int y) {
+        yPos = y;
+    }
 
-	public int getY()
-	{
-		return 0;  //finish this method
-	}
+    public int getX() {
+        return xPos;
+    }
 
-	public void setWidth(int w)
-	{
-		//add code here
-	}
+    public int getY() {
+        return yPos;
+    }
 
-	public void setHeight(int h)
-	{
-		//add code here
-	}
+    public void setWidth(int w) {
+        width = w;
+    }
 
-	public int getWidth()
-	{
-		return 0;  //finish this method
-	}
+    public void setHeight(int h) {
+        height = h;
+    }
 
-	public int getHeight()
-	{
-		return 0;  //finish this method
-	}
+    public int getWidth() {
+        return width;
+    }
 
-	public abstract void move(String direction);
-	public abstract void draw(Graphics window);
+    public int getHeight() {
+        return height;
+    }
 
-	public String toString()
-	{
-		return getX() + " " + getY() + " " + getWidth() + " " + getHeight();
-	}
+    public abstract void move(String direction);
+
+    public abstract void draw(Graphics window);
+
+    public String toString() {
+        return getX() + " " + getY() + " " + getWidth() + " " + getHeight();
+    }
 }
