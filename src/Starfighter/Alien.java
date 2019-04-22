6e8bc1fdda7e5922ca6e08e9e9ba902b969967de
dyaom <dyaomaster@yahoo.com>
2019-04-22 09:07:06 -0700

    the Ailen class is basically identical to the Ship class

--- src/Starfighter/Alien.java
+++ src/Starfighter/Alien.java
@@ -3,7 +3,6 @@ package Starfighter;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.io.File;
 import java.net.URL;
 import java.awt.Color;
@@ -11,63 +10,66 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 
-public class Alien extends MovingThing
-{
-	private int speed;
-	private Image image;
+public class Alien extends MovingThing {
 
-	public Alien()
-	{
-		this(0,0,30,30,0);
-	}
+    private int speed;
+    private Image image;
 
-	public Alien(int x, int y)
-	{
-		//add code here
-	}
+    public Alien() {
+        this(0, 0, 30, 30, 0);
+    }
 
-	public Alien(int x, int y, int s)
-	{
-		//add code here
-	}
+    public Alien(int x, int y) {
+        this(x, y, 30, 30, 0);
+    }
 
-	public Alien(int x, int y, int w, int h, int s)
-	{
-		super(x, y, w,h);
-		speed=s;
-		try
-		{
-			URL url = getClass().getResource("/images/alien.jpg");
-			image = ImageIO.read(url);
-		}
-		catch(Exception e)
-		{
-			//feel free to do something here
-		}
-	}
+    public Alien(int x, int y, int s) {
+        this(x, y, 30, 30, s);
+    }
 
-	public void setSpeed(int s)
-	{
-	   //add code
-	}
+    public Alien(int x, int y, int w, int h, int s) {
+        super(x, y, w, h);
+        speed = s;
+        try {
+            URL url = getClass().getResource("images/alien.jpg");
+            image = ImageIO.read(url);
+        } catch (Exception e) {
+            System.out.println("alright who deleted the ailens");
+        }
+    }
 
-	public int getSpeed()
-	{
-	   return 0;
-	}
+    public void setSpeed(int s) {
+        speed = s;
+    }
 
-   public void move(String direction)
-	{
-	   //add code here
-	}
+    public int getSpeed() {
+        return speed;
+    }
 
-	public void draw( Graphics window )
-	{
-   	window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
-	}
+    public void move(String dir) {
+        switch (dir) {
+            case "LEFT":
+                setX(getX() - speed);
+                break;
+            case "RIGHT":
+                setX(getX() + speed);
+                break;
+            case "UP":
+                setY(getY() - speed);
+                break;
+            case "DOWN":
+                setY(getY() + speed);
+                break;
+        }
+    }
 
-	public String toString()
-	{
-		return "";
-	}
+	
+
+    public void draw(Graphics window) {
+        window.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
+    }
+
+    public String toString() {
+        return "";
+    }
 }
